09e4aa71f66da04ee0b2993516700f9a15810e46
mojogit <64355944+mojogit@users.noreply.github.com>
2020-07-24 16:13:44 +0500

    Add files via upload
    please

---
+++ My ass 5/app.js
@@ -0,0 +1,332 @@
+//chap#1//
+1.
+ //alert("Hi,welcome to my website.Login please") //
+
+ 2.
+
+//alert("Error! Please enter a valid code.")
+
+3.
+ //alert("Welcome to js land <br> Happy coding")
+ 4.
+//alert("Welcome to js land.")
+//alert("Happy coding")
+5.
+//done check my web:
+//https://mojophotography-233.web.app/
+
+
+//Chap#2
+1,2.
+//var username = Maaz safyan
+//var fullname = 
+//alert(username + fullname)
+
+3.
+//var message = "Hello World"
+//alert(message)
+4.
+// var name = "Jhone doe"
+//var age = "15 years old"
+//var quali = "Certified mobile app"
+//alert(name)
+//alert(age)
+//alert(quali)
+5.
+//var all = "pizza " <br/>  "pizza "
+//alert(all)
+6.
+//var email = "email addrres is :"
+//alert(email + "Maazsaf32@hotmail.com")
+7.
+//var book = "A smarter way to learn java script"
+//alert("i am trying to learn " + book)
+8.
+//document.write("ya i can do it .Dw about me.")
+9.
+//var z ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” " 
+//alert(z)
+
+
+//Chap #3
+
+1. 
+//var age = "i am 15 years old."
+//alert(age)
+2.
+//var time = "21"
+//alert("You have visited this site" + time + "times.")
+3.
+//var birth = "27/11/2006"
+//document.write("My DOB is  " + birth)
+4.
+//var vname = "john doe"
+//var product = "T-shirts"
+//var no = " 5 "
+//document.write(name + " ordered" + no + product + "On XYz clothing store. ")
+
+//Chap #4
+
+//chap#5
+1.
+//var a = 10
+//var b = 5
+//var c = a + b
+//document.write("The sum of 10 + 5 is" + c)
+2.
+//var a = 10
+//var b = 5
+//var c = a * b
+//document.write("The sum of 10 x 5 is  " + c)
+
+3.
+//var any = 5
+//document.write(" The value after declartion is:" + any) 
+//var ini = 4
+//document.write(" The intialization value is: " + ini) 
+//var bos = any + ini
+//document.write(" Value after increment is: " + bos ) 
+//var bos1 = bos + 7
+//document.write(" Value after addition is:" + bos1)
+//var so = bos1 - 3
+//document.write(" Value after decrement :" + so)
+//var fin = so/3
+//document.write("  dividing" + so + "with 3")
+//document.write(" remaninder is " + fin)
+
+4.
+//var ticket = 600
+//var people = 5
+//var all = ticket * people
+//document.write("total cost to buy " + people + "tickets is " + all)
+
+6.
+//var p1 = 650 ;
+//var pq1 = 3;
+//var p2 = 100;
+//var pq2 = 7;
+//var ship = 100;
+//document.write("item 1 price is:  " + p1 ) 
+//document.write("  .item 1 quantity is : " + pq1)
+//document.write(" .item 2 price is: " + p2 )
+//document.write(" .item 2 quantity is: " + pq2)
+//document.write(" .Shipping cost " + ship)
+//document.write(  .p1* pq1 + p2 * pq2 + ship )
+7.
+
+//var total = 980
+//var obtained = 804
+//var Percentage = 100
+//document.write("<h1> Mark sheet</h1>")
+//document.write(" Total Marks:" + total)
+//document.write(" Obtained marks :" + obtained)
+//document.write(" Percentage :" + obtained/total * Percentage)
+
+8.
+//document.write("in pakistani rupees is" + ( 104.80 * 10) + (28 * 25 )   )
+
+9.
+//var first = 6; 
+//document.write( (first + 6 ) * (10) / (2))
+10.
+//document.write("<h1> Age calculator</h1>")
+//var now = 2016
+//var year = 1992
+//document.write(" Now year :" + now  ) 
+//document.write(" Dob : " + year)
+//document.write(" Your age is :" + (now - year) )
+11.,12
+//sir jee said to skip
+13.
+//sir samaj nhi aya
+
+
+//chap # 6-9
+
+1.
+//var a = 10;
+//var b = ++a;
+//var c = b++
+//var d = --a
+//document.write(" Orignal value = : 10"  ) 
+//document.write( "Value after ++a is :" + b )
+//document.write(" Now the vlaue is :" + b)
+//document.write(" Value after a++ is:" + c)
+//document.write(" Now the vlaue is : 12")
+//document.write("the value after --a is" + d )
+
+2.
+//var a = 2, b = 1; 
+//var result = --a - --b + ++b + b--; 
+//document.write(" the value a is = " + result)
+3.
+//var name = window. prompt("Enter your name: "); 
+//alert("Welcome to my website  " + name); 
+4.
+//N/A
+5. //skip
+
+//chap#9-11
+
+1.
+//var name = window. prompt("Enter City name: "); 
+//if (name === "karachi") {
+ //   alert("Welcome to the city of lights!")
+//} else {
+//    alert("Soory nit in karachi.")
+//}
+2.
+//var sum = prompt("Enter Yr Gender")
+//if (sum === "male") {
+//	alert("good morning sir !")
+//} else {
+//	alert("good morning miss!")
+//}
+
+
+
+4.
+//var fuel = prompt("enter fuel.")
+//if (fuel < 0.25 ) {
+//    alert("Get gas fill immedialtely.")
+//} else {
+//    alert("no need fueling right now.")
+//}
+
+5.
+//No not working
+
+6.
+7.
+//var number = prompt("Think of my lucky No.");
+//if (number === "7") {
+  //  alert("Bingo!")
+//} 
+//if (number === "8") {
+ //   alert("Very close")
+//} 
+ //else {
+ //   alert("No wrong no.")
+    
+//}
+8.
+//var number = prompt("Type any number to check if its divible by 3 (only no. till 60)")
+//if (number ==("3", "6", "9", "12", "15", "18", "21", "24", "27", "30","33","36","39","42","45","48","51","54","57","60" )) {
+ //   alert("Yes"  +   number   + "is divisible by 3")
+//} else {
+ //   alert(" no,it cannot be divided by 3 ")
+    
+//}
+
+9.
+//var num = prompt("enter a number")
+//if ( num % 2 === 0) {
+//	alert('Even Number');
+//}else{
+//	alert('Odd Number');
+//}
+
+// var T = prompt("Enter Temperature in yr area.(In celcius and without symbol)")
+//if (T > "40") {
+  //  alert("It is too hot outside")
+//}
+//if (T > "30" ) {
+//    alert("The Weather today is Normal.")
+//}
+//if (T > "20") {
+//    alert("Today’s Weather is cool")
+//} 
+//if (T > "10") {
+//    alert("“Omg! Today’s weather is so Cool")
+//}
+
+//Imp note to sir and saylani team:
+//AOa sir my name is maaz and  im 14 years old,Along with this course i have tuition and school on zoom aswell.
+//First i thought of quiting bcz of school and stuff,but the motivation you gave was amazing.
+//Sir i only request onething.Pls accept this right now.I promise you to be good at it one day
+// . Actully im new to this and many problems are there. But i will IA overcome those.
+
+// 21 to further
+
+// var first = prompt("Please enter your first name");
+// var second = prompt("Please enter your second name name") ;
+// var all = alert("Welcome " + first +(" ") + second + " to our website")
+
+// var phone = prompt("Sir what is your fav phone?")
+// var cd = document.write("your favourite phone is " + phone)
+
+// var bbaa = pakistan;
+// // console.log(indexoff(bbaa))
+
+// sir i didnt do all
+
+// Dear sir,Assalamoalikum.Sir i have contacted you both before also regarding this course.
+// My name is maaz and I'm 14 years old only.I joined this web development because I was free and wanted 
+// to learn something new.I didn't know that it was for 6 months.So sir now my parents are saying Me to 
+// leave this and focus on my studies.I SIMPLY CANNOT SIR,because all the motivation you have given me.
+// Now my exam week has started and it has become very difficult for me to do the assignments with the test.
+// And I know that if I leave the assignment ill be a problem because I wont get the cisco certificate.
+// I am a school going student in gr 8 and everyday I have online classes on zoom.Now sir you tell what to do.
+// My parents say that I can do this stuff when I grow up,but I want to learn now.I know it will affect my studies but.
+// Now sir could you suggest me that what to do?Will be another course held like this in future? 
+// Am I capable @age of 14?S Should I Continue? I hope a reply from you. waise I'm having lil confusions in js but
+//  I'll overcome.Html and css was taught perfectly and now I can at least make a static web. I have no intention
+//   to do freelancing right now,but just wanted to get hold on a skill.Pleae do reply SIR! I will try to do some of them...
+
+// I AM DOUNG LESS BCZ EXAMZ
+// NO one listens i have messaged to you all but got no response
+// 26-30
+
+// var num = prompt("Enter a no. with Decimal");
+// alert(num.round(1))
+
+// i dont know parse meaning
+// var wei = prompt("Enter your weight");
+// var all = alert("your weight is" +(" ") + wei);
+
+// var asas = prompt("Type a number from 1 to 10 , and check your lucky no.");
+// if (asas === "7") {
+//   alert("Congrats you typed the lucky no.");
+// } else {
+//   alert("Try Again") ;
+// }
+// 31-34
+
+// var we = new Date();
+// alert(we)
+
+// var mon = getMonth();
+// alert("The current Month is " + (" ") + mon )
+
+
+// I AM DOING QUICLKY 
+
+
+// function gettime() {
+//   new Date()
+  
+// }
+// Calculator :
+
+// function getallnum(num){
+//   var result = document.getElementById("result")
+//   result.value += num;
+
+// }
+
+// function clearResult(){
+//   var result = document.getElementById("result");
+//   result.value = ""
+// }
+
+// function getResult(){
+//   var result = document.getElementById("result")
+// result.value = eval(result.value)
+// }
+
+
+// This is on html side:
+
+
+
+
